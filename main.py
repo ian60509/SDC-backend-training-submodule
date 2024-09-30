e33bb791896564f6acd543639d1be7994b4abde9
ian60509 <ian.60509@gmail.com>
2024-09-30 11:06:15 +0000

    feat: finish HW2

--- main.py
+++ main.py
@@ -1,7 +1,27 @@
-from fastapi import FastAPI
+from typing import Annotated
+
+from fastapi import FastAPI, Path
+from pydantic import BaseModel
 
 app = FastAPI()
 
+class Item(BaseModel):
+    name: str
+    description: str = None
+    price: float
+    tax: float 
+
+
 @app.get("/")
-async def root():
+async def health_check():
     return {"message": "Hello World"}
+
+@app.put("/items/{item_id}")
+async def put_item(item_id: int , 
+                    q: str | None = None,
+                    item: Item = None):
+    res = {"item_id": item_id, **item.dict()}
+    if q:
+        return {**res, "q": q}
+    else :
+        return res
