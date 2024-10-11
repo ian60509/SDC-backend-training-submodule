5c53407923a87cc5f10eea4c4d914306d912ced2
ian60509 <ian.60509@gmail.com>
2024-10-11 12:42:46 +0000

    feat: finish HW3

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 from typing import Annotated
 
-from fastapi import FastAPI, Path
+from fastapi import FastAPI, Path, Query
 from pydantic import BaseModel
+from enum import Enum
 
 app = FastAPI()
+
+class SortOrder(str, Enum):
+    asc = "asc"
+    desc = "desc"
 
 class Item(BaseModel):
     name: str
@@ -16,12 +21,32 @@ class Item(BaseModel):
 async def health_check():
     return {"message": "Hello World"}
 
+
 @app.put("/items/{item_id}")
-async def put_item(item_id: int , 
-                    q: str | None = None,
+async def put_item(item_id: Annotated[int, Path(ge=1, le=1000)], 
+                    q: Annotated[str|None, Query(max_length=50, min_length=3)] = None,
                     item: Item = None):
     res = {"item_id": item_id, **item.dict()}
     if q:
         return {**res, "q": q}
     else :
         return res
+
+@app.get("/items/{item_id}")
+async def get_item(
+    item_id: Annotated[int, Path(ge=1, le=1000)], 
+    q: Annotated[str|None, Query(max_length=50, min_length=3)] = None, 
+    sort_order: SortOrder = SortOrder.asc):
+    if q:
+        return {
+            "item_id": item_id, 
+            "description": "This is a sample item that matches the query test_query",
+            "sort_order": sort_order
+        }
+    else:
+        return {
+            "item_id": item_id,
+            "description": "This is a sample item.",
+            "sort_order": sort_order
+        }
+    return {"item_id": item_id, "q": q}
