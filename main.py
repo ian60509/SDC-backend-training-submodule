346aaaa3938cf9af1866c7a41fe62341ae619640
ian60509 <ian.60509@gmail.com>
2024-11-04 12:14:29 +0000

    finish HW4

--- main.py
+++ main.py
@@ -1,32 +1,35 @@
 from typing import Annotated
 
-from fastapi import FastAPI, Path, Query
+from datetime import datetime, timedelta
+
+from fastapi import Body, FastAPI
+from fastapi import Query, Path
 from pydantic import BaseModel
+from fastapi import Cookie
+from fastapi import Header
 from enum import Enum
 
 app = FastAPI()
+
+class Item(BaseModel):
+    name: str
+    description: str = None
+    price: float = None
+    tax: float = None
 
 class SortOrder(str, Enum):
     asc = "asc"
     desc = "desc"
 
-class Item(BaseModel):
-    name: str
-    description: str = None
-    price: float
-    tax: float 
-
-
 @app.get("/")
-async def health_check():
+async def root():
     return {"message": "Hello World"}
-
 
 @app.put("/items/{item_id}")
 async def put_item(item_id: Annotated[int, Path(ge=1, le=1000)], 
                     q: Annotated[str|None, Query(max_length=50, min_length=3)] = None,
                     item: Item = None):
-    res = {"item_id": item_id, **item.dict()}
+    res = {"item_id": item_id, **item.model_dump()}
     if q:
         return {**res, "q": q}
     else :
@@ -50,3 +53,63 @@ async def get_item(
             "sort_order": sort_order
         }
     return {"item_id": item_id, "q": q}
+
+
+@app.post("/items/filter/")
+async def read_items(
+    price_min: int,
+    price_max: int,
+    tax_included: bool ,
+    tags: list[str] = Query(None),
+):
+    return {"price_range": [price_min, price_max], 
+            "tax_included": tax_included, 
+            "tags": tags,
+            "message": "This is a filtered list of items based on the provided criteria."}
+
+@app.post("/items/create_with_fields/")
+async def create_item_with_fields(
+    item: Item, 
+    importance: Annotated[int, Body()],
+):
+    return {"item": item, "importance": importance}
+
+@app.post("/offers/")
+async def create_offer(
+    name: Annotated[str, Body()],
+    discount: Annotated[float, Body()], # the The discount percentage for the offer.
+    items: Annotated[list[Item], Body()], # A list of items included in the offer.
+):
+    return {
+        "offer_name": name, 
+        "discount": discount, 
+        "items": items}
+
+@app.post("/users/")
+async def create_user(
+    username: Annotated[str, Body()], # Username of the user.
+    email: Annotated[str, Body()], # Email of the user.
+    full_name: Annotated[str, Body()], # Full name of the user.
+):
+    return {"username": username, "email": email, "full_name": full_name}
+
+@app.post("/items/extra_data_types/")
+async def read_extra_data_types(
+    start_time: Annotated[datetime, Body()], # Start time of the item's availability.
+    end_time: Annotated[str, Body()], # End time of the item's availability.
+    repeat_every: Annotated[timedelta, Body()], # Time interval for repeating the item.
+    process_id: Annotated[str, Body()], # Unique identifier for the process.
+):
+    return {
+            "message": "This is an item with extra data types.", 
+            "process_id": process_id
+    }
+
+@app.get("/items/cookies/")
+async def read_items_cookie(
+    session_id: Annotated[str, Cookie()], # Session ID from the client's cookies.
+):
+    return {
+        "session_id": session_id, 
+        "message": "This is the session ID obtained from the cookies."
+    }
